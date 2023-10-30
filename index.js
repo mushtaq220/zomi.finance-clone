32dd6b7cf8fa5467ccfe821b6b1bb21628e009b7
mushtaq220 <mushtaqansari220@gmail.com>
2023-10-30 16:27:44 +0530

    completed

---
+++ index.js
@@ -0,0 +1,9 @@
+const ethereumBtn = document.getElementById("ethereum-btn");
+const dropdownBtn = document.getElementById("dropdown");
+
+ethereumBtn.addEventListener("click", () => {
+
+    dropdownBtn.classList.toggle("invisible");
+
+
+});
